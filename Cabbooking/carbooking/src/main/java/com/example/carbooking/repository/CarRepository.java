a9d18bf12bf01da128b326c50503dfb47aad2c2f
Javaspring-dev <jtraining32@gmail.com>
2024-12-12 18:54:23 +0530

    Delete Cabbooking/carbooking directory

--- Cabbooking/carbooking/src/main/java/com/example/carbooking/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.carbooking.repository;
-
-import com.example.carbooking.entities.CarEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<CarEntity,Long> {
-}
